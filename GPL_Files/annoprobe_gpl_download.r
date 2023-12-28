59c32cf98e5365f7f8ef6980fc861b06d8f04df7
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2023-12-28 11:38:32 -0500

    Update annoprobe_gpl_download.r

--- GPL_Files/annoprobe_gpl_download.r
+++ GPL_Files/annoprobe_gpl_download.r
@@ -1,26 +1,26 @@
-install.packages("magrittr")
-
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
+#install.packages("magrittr")
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
 #BiocManager::install("GEOquery")
-
-#library(devtools)
 #install_github("jmzeng1314/AnnoProbe")
+
+library(devtools)
+
 library(AnnoProbe)
 gpls = getGPLList()[1]
 
 for (i in 1:length(gpls[,1])) {
   print(i)
   gpl = gpls[i,]
+  gpl
   print(gpl)
   tryCatch(
     
     expr = {
       full_table = "";
-      full_table <- idmap(gpl);
+      full_table <- idmap(gpl, type='soft');
       filename = paste("/Users/4472414/Documents/Current_Manuscripts/GSE_Download_Tools/GPLs/", gpl, ".txt", sep="");
+      #filename = paste("./", gpl, ".txt", sep="");
       write.table(full_table, file=filename, col.names=NA, sep="\t");
       
     },
@@ -31,5 +31,50 @@ for (i in 1:length(gpls[,1])) {
   )
   
 }
-a
-getwd()
+
+for (i in 1:length(gpls[,1])) {
+  print(i)
+  gpl = gpls[i,]
+  gpl
+  print(gpl)
+  tryCatch(
+    
+    expr = {
+      full_table = "";
+      full_table <- idmap(gpl, type='bioc');
+      filename = paste("/Users/4472414/Documents/Current_Manuscripts/GSE_Download_Tools/GPLs/", gpl, ".txt", sep="");
+      #filename = paste("./", gpl, ".txt", sep="");
+      write.table(full_table, file=filename, col.names=NA, sep="\t");
+      
+    },
+    error = function(e){
+      message('Caught an error!')
+      print(e)
+    }
+  )
+  
+}
+
+for (i in 1:length(gpls[,1])) {
+  print(i)
+  gpl = gpls[i,]
+  gpl
+  print(gpl)
+  tryCatch(
+    
+    expr = {
+      full_table = "";
+      full_table <- idmap(gpl, type='soft');
+      filename = paste("/Users/4472414/Documents/Current_Manuscripts/GSE_Download_Tools/GPLs/", gpl, ".txt", sep="");
+      #filename = paste("./", gpl, ".txt", sep="");
+      write.table(full_table, file=filename, col.names=NA, sep="\t");
+      
+    },
+    error = function(e){
+      message('Caught an error!')
+      print(e)
+    }
+  )
+  
+}
+
